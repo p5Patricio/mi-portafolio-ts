f57a5a31c957430310e3b1f02a3df828f8c32f68
p5Patricio <pa.garciaperezvela@ugto.mx>
2025-06-15 20:11:56 -0600

    Estilos y fondo terminados y funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css'; 
-import logo from './assets/LogosinFondo.png'; 
 import AppContent from './AppContent';
+import Particles from './components/Particles';
+import { ThemeContext } from './context/ThemeContext';
+import logoDarkTheme from './assets/LogoDark.png'; 
+import logoLightTheme from './assets/LogoLight.png';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isFinishing, setIsFinishing] = useState(false);
+
+  const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     // ... (la lógica de los temporizadores sigue igual)
@@ -23,10 +28,24 @@ function App() {
     };
   }, []);
 
+  const darkThemeParticleColors = ['#61dafb', '#ffffff']; // Azul y blanco para el tema oscuro
+  const lightThemeParticleColors = ['#000000', '#61dafb']; // Negro y un gris oscuro para el tema claro
+
   return (
     <div className="app-wrapper">
+        <Particles
+          particleColors={theme === 'dark' ? darkThemeParticleColors : lightThemeParticleColors}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={false}
+          alphaParticles={false}
+          disableRotation={false}
+        />
       <img
-        src={logo}
+        // Usamos un operador ternario para decidir qué logo mostrar
+        src={theme === 'dark' ? logoDarkTheme : logoLightTheme}
         className={`persistent-logo ${isFinishing ? 'finished' : ''}`}
         alt="Logo principal de mi portafolio"
       />
